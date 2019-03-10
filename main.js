576ca0836a265a32b929e63598318d267b79f35c
Santiago Casar <santiago@nowhereprod.com>
2019-03-10 14:50:47 -0400

    added extra challenge to get the address from reverse geo-location google API

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const state_array = [];
 
 $('.state-input').on('click',function(){
   event.preventDefault();
+  $(".multiple-state").val("");
+  $('.parks-container').empty()
   const state_values = $('.multiple-state')
   for (let i =0; i<state_values[0].selectedOptions.length;i++){
   const state_selection = state_values[0].selectedOptions[i].computedName
@@ -24,7 +26,10 @@ $('.state-input').on('click',function(){
 })
 
 function getParks(state_string,max_results){
-  var request = new Request('https://developer.nps.gov/api/v1/parks?stateCode=CA', {
+  let limit_results = max_results -1
+  const geocordinates = []
+
+  let request = new Request(`https://developer.nps.gov/api/v1/parks?stateCode=${state_string}+&limit=+${limit_results}`, {
   method: 'GET',
 	mode: 'cors',
 	headers: new Headers({
@@ -36,8 +41,56 @@ function getParks(state_string,max_results){
     return answer
     .then(function(answer){
       console.log(answer.data)
+      for(let i=0;i<answer.data.length;i++){
+      $('.parks-container').append(`
+        <h1>${answer.data[i].fullName}</h1>
+        <p>${answer.data[i].description}</p>
+        <h2>${answer.data[i].url}</h2>
+        <h3 class=${i}address></h3>
+        `)
+        geocordinates.push(answer.data[i].latLong)
+
+      }
+      getAdress(geocordinates)
+
     })
 
   })
 
+}
+
+function getAdress(geocordinates){
+  console.log(geocordinates)
+  for (let i = 0; i<geocordinates.length;i++){
+    // console.log(geocordinates[i].toString())
+    let latitude = geocordinates[i].replace('lat:','');
+    let latitude_only = latitude.split(',')[0]
+
+
+    let longitude = geocordinates[i].replace('long:','');
+    let longitude_only = longitude.substring(longitude.indexOf(",")+1)
+    let latitude_final = latitude_only.replace(' ','')
+    let longitude_final = longitude_only.replace(' ','')
+    // let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude_final},${longitude_final}`
+    // let string_url = url.toString()
+    // string_url.replace(/\s/g, "")
+    // console.log(url)
+    let request = new Request(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude_final},${longitude_final}&key=AIzaSyDNKJqTTO1ABklWksGvydQ0X2nCpM4yRNY`, {
+    method: 'GET',
+    mode: 'cors',
+    headers: new Headers({
+    })
+  });
+
+  fetch(request).then(function(response){
+    console.log(request)
+    let answer = response.json()
+    return answer
+    .then(function(answer){
+      let address = answer.results[0].formatted_address
+      $(`.${i}address`).append(
+      `${address}`)
+    })
+  })
+  }
 }
