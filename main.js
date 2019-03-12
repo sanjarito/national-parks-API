aeaa201a0201f08e24a7b7044bba6513eea41274
Santiago Casar <santiago@nowhereprod.com>
2019-03-12 19:36:21 -0400

    commit all

--- main.js
+++ main.js
@@ -1,91 +1,52 @@
-// console.log('hello')
 'use strict';
-
 
 $('.state-input').on('click',function(event){
   event.preventDefault()
   let state_array = [];
   $('.parks-container').empty()
   const state_values = $('.multiple-state')
-
   for (let i =0; i<state_values[0].selectedOptions.length;i++){
-
-  let state_selection = state_values[0].selectedOptions[i].value
-  // console.log(state_selection)
-  state_array.push(state_selection)
-  }
-  // console.log(state_array)
+    let state_selection = state_values[0].selectedOptions[i].value
+    state_array.push(state_selection)
+    }
   const max_results = $('.max-results').val()
-  // console.log(state_array)
-
-
   let state_string = state_array.toString()
-  console.log(state_string)
-
-
-
-
   getParks(state_string,max_results)
-
-
-  // $('.state-input').on('click',function(){
-  //   console.log(state_selection)
-  //
-  // })
 })
 
 function getParks(state_string,max_results){
   console.log('getparks function debug')
   let limit_results = max_results -1
   const geocordinates = []
-
   let parks_url = `https://developer.nps.gov/api/v1/parks`
-  let parks_url2 = `https://developer.nps.gov/api/v1/parks?api_key=3CXESj6FPvSAowQ1dKe2xVYlUV5GQIb6Q5E5ecGy&stateCode=${state_string}+&limit=+${limit_results}`
-//   let request = new Request(parks_url, {
-//   method: 'GET',
-// 	mode: 'cors',
-// 	headers: new Headers({
-// 		"Authorization": '3CXESj6FPvSAowQ1dKe2xVYlUV5GQIb6Q5E5ecGy'
-// 	})
-// });
+  let parks_url2 = `https://api.nps.gov/api/v1/parks?api_key=3CXESj6FPvSAowQ1dKe2xVYlUV5GQIb6Q5E5ecGy&stateCode=${state_string}&limit=${limit_results}`
   fetch(parks_url2)
   .then(function(response){
     let answer = response.json()
     return answer
     .then(function(answer){
       for(let i=0;i<answer.data.length;i++){
-      $('.parks-container').append(`
-        <h1>${answer.data[i].fullName}</h1>
-        <p>${answer.data[i].description}</p>
-        <h2>${answer.data[i].url}</h2>
-        <h3 class=${i}address></h3>
-        `)
-        geocordinates.push(answer.data[i].latLong)
-
-      }
-      getAdress(geocordinates)
-
+        $('.parks-container').append(`
+          <h1>${answer.data[i].fullName}</h1>
+          <p>${answer.data[i].description}</p>
+          <h2>${answer.data[i].url}</h2>
+          <h3 class=${i}address></h3>
+          `)
+          geocordinates.push(answer.data[i].latLong)
+        }
+    getAdress(geocordinates)
     })
-
   })
-
 }
 
 function getAdress(geocordinates){
   for (let i = 0; i<geocordinates.length;i++){
-    // console.log(geocordinates[i].toString())
     let latitude = geocordinates[i].replace('lat:','');
     let latitude_only = latitude.split(',')[0]
-
-
     let longitude = geocordinates[i].replace('long:','');
     let longitude_only = longitude.substring(longitude.indexOf(",")+1)
     let latitude_final = latitude_only.replace(' ','')
     let longitude_final = longitude_only.replace(' ','')
-    // let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude_final},${longitude_final}`
-    // let string_url = url.toString()
-    // string_url.replace(/\s/g, "")
-    // console.log(url)
     let request = new Request(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude_final},${longitude_final}&key=AIzaSyDNKJqTTO1ABklWksGvydQ0X2nCpM4yRNY`, {
     method: 'GET',
     mode: 'cors',
